4d46036a8e88f67c245cd3df2d7171d591e97c45
wasabi07-git <187173963+wasabi07-git@users.noreply.github.com>
2025-03-20 15:45:19 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if (\APP::enviroment(['production'])) {
+            \URL::forseScheme('https');
+        }
     }
 }
