558760f0ff997740f046475ea8bc55bab4cd29f3
wasabi07-git <187173963+wasabi07-git@users.noreply.github.com>
2025-03-24 00:11:16 +0900

    削除機能の修正

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -29,32 +29,13 @@
                     <td>{{ $admin->email }}</td>
                     <td>
                         <a href="{{ route('admin.edit', $admin->id) }}" class="btn btn-warning">編集</a>
-                        <button class="btn btn-danger" data-toggle="modal" data-target="#deleteModal{{ $admin->id }}">削除</button>
                         
-                        <!-- 削除モーダル -->
-                        <div class="modal fade" id="deleteModal{{ $admin->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">削除確認</h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-                                        本当にこの管理者を削除しますか？
-                                    </div>
-                                    <div class="modal-footer">
-                                        <form action="{{ route('admin.destroy', $admin->id) }}" method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">キャンセル</button>
-                                            <button type="submit" class="btn btn-danger">削除</button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <!-- 削除フォーム -->
+                        <form action="{{ route('admin.destroy', $admin->id) }}" method="POST" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger" onclick="return confirm('本当にこの管理者を削除しますか？');">削除</button>
+                        </form>
                     </td>
                 </tr>
             @endforeach
